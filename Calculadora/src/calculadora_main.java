29b4d026d492e7f38710dfcb2576187dad33dbc8
CaptainTrekstor <oliveira.jorge@ua.pt>
2017-02-04 18:11:13 +0000

    Pushing calculator_class

---
+++ Calculadora/src/calculadora_main.java
@@ -0,0 +1,14 @@
+/**
+ * Created by captaintrekstor on 04-02-2017.
+ *
+ * Bom dia Jose
+ * E suposto fazeres uma calculadora
+ * Sugiro que des uma olhadela em:
+ * Como fazer uma classe
+ * Como funciona um switch
+ * Como funcionam os operadores
+ *
+ */
+
+public class calculadora_main {
+}
