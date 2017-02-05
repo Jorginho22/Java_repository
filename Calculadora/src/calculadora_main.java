13c84542a0c6feacab062ef76dd6ebacec15745c
CaptainTrekstor <oliveira.jorge@ua.pt>
2017-02-05 00:07:02 +0000

    Calculator project update

--- Calculadora/src/calculadora_main.java
+++ Calculadora/src/calculadora_main.java
@@ -1,14 +1,12 @@
 /**
  * Created by captaintrekstor on 04-02-2017.
- *
- * Bom dia Jose
- * E suposto fazeres uma calculadora
- * Sugiro que des uma olhadela em:
- * Como fazer uma classe
- * Como funciona um switch
- * Como funcionam os operadores
- *
  */
-
 public class calculadora_main {
+
+    public static void main(String[] args){
+
+        System.out.print("Hello World");
+
+    }
+
 }
